fea9160019d211fc769c31fff2768b70e8f55ab3
samuelccp0161 <samuelccp0161@gmail.com>
2023-01-24 08:00:13 -0300

    primeira atividade do curso de java para iniciante até o nivel experiente.

--- untitled/src/main/java/exercicio2/QuestaoCinco.java
+++ untitled/src/main/java/exercicio2/QuestaoCinco.java
@@ -7,13 +7,28 @@ public class QuestaoCinco {
         Scanner scanner = new Scanner(System.in);
 
         int cod;
+        int quantidade;
+
         cod = scanner.nextInt();
+        quantidade = scanner.nextInt();
 
-
-
-        int valor;
-        valor
-
+        if (cod == 1){
+            Double valor = 4.00;
+            System.out.println(quantidade * valor);
+        }if (cod == 2){
+            Double valor = 4.50;
+            System.out.println(quantidade * valor);
+        }if (cod == 3){
+            Double valor = 5.00;
+            System.out.println(quantidade * valor);
+        }if (cod == 4){
+            Double valor = 2.00;
+            System.out.println(quantidade * valor);
+        }if(cod == 5){
+            Double valor = 1.50;
+            System.out.println(quantidade * valor);
+        }else
+            System.out.println("Por favor Digite um codigo valido");
 
         scanner.close();
     }
