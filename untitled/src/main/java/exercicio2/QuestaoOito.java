730d8a11355b54effbe2a228e837d2eab44c140a
sdcabral <sdcabral@sefaz.al.gov.br>
2023-01-27 11:27:02 -0300

    exercicio finalizado sobre while comentando algumas coisas.

--- untitled/src/main/java/exercicio2/QuestaoOito.java
+++ untitled/src/main/java/exercicio2/QuestaoOito.java
@@ -17,10 +17,13 @@ public class QuestaoOito {
             return "Isento";
         } else if (salario <= 3000.00) {
             return (salario - 2000) * 0.08 + "" ;
+            //8% de (salario - 2000).
         } else if (salario <= 4500.00){
             return (salario - 3000) * 0.18 + 80 + "";
+            //18% de (salario - 3000) + 80 (valor da primeira taxa).
         }else if (salario > 4500){
             return (salario - 4500) * 0.28 + 80 + 270 + "";
+            //28% de (salario - 4500) + 80 (valor da primeira taxa), 270 (valor da segunda taxa).
         }
         return "";
     }
